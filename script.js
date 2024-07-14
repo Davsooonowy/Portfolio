40a560ba9d5bb66a5f0bac32b223b1a0253bda9f
Dawid Mularczyk <72767335+Davsooonowy@users.noreply.github.com>
2024-07-14 15:51:20 +0200

    refactor and added some new skills and projects

--- script.js
+++ script.js
@@ -9,29 +9,31 @@ function handleVisibilityChange() {
         document.title = pageTitle;
     }
 }
+
 document.addEventListener("visibilitychange", handleVisibilityChange);
 
 
 /*===== MENU SHOW =====*/
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show')
 }
+
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
@@ -58,21 +60,22 @@ setInterval(textLoad, 16000);
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
         }
     })
 }
+
 window.addEventListener('scroll', scrollActive)
 
 
@@ -106,21 +109,21 @@ const sr = ScrollReveal({
     delay: 200,
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 200});
-sr.reveal('.home__social-icon',{ interval: 200});
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200});
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', {delay: 200});
+sr.reveal('.home__social-icon', {interval: 200});
+sr.reveal('.skills__data, .work__img, .contact__input', {interval: 200});
 sr.reveal('.home__title', {});
-sr.reveal('.button', { delay: 200 });
-sr.reveal('.home__social-icon', { interval: 200 });
-sr.reveal('.about__subtitle, .about__text', { delay: 300 });
-sr.reveal('.skills__icon, .skills__name', { interval: 150 });
-sr.reveal('.toggle_switch_container', { delay: 200 });
-sr.reveal('.timeline', { delay: 200 });
-sr.reveal('.box', { interval: 200 });
-sr.reveal('.contact__input', { interval: 200 });
-sr.reveal('.section_description', { interval: 200 });
-sr.reveal('.contact__info, .contact__title, .contact__content, .info', { interval: 200 });
+sr.reveal('.button', {delay: 200});
+sr.reveal('.home__social-icon', {interval: 200});
+sr.reveal('.about__subtitle, .about__text', {delay: 300});
+sr.reveal('.skills__icon, .skills__name', {interval: 150});
+sr.reveal('.toggle_switch_container', {delay: 200});
+sr.reveal('.timeline', {delay: 200});
+sr.reveal('.box', {interval: 200});
+sr.reveal('.contact__input', {interval: 200});
+sr.reveal('.section_description', {interval: 200});
+sr.reveal('.contact__info, .contact__title, .contact__content, .info', {interval: 200});
 
 /*==================== CONTACT FORM ====================*/
 function validateForm() {
@@ -162,7 +165,7 @@ function handleToggleSwitchChange() {
                 '                <div class="content">\n' +
                 '                    <div class ="content_date">\n' +
                 '                        <span class="date-company-large">Lekta AI</span>\n' +
-                '                        <span class="date-company-small">March 2024 - present</span>\n' +
+                '                        <span class="date-company-small">March 2024 - July 2024</span>\n' +
                 '                    </div>\n' +
                 '                    <h2>Junior Software Developer</h2>\n' +
                 '                    <p>\n' +
@@ -170,7 +173,7 @@ function handleToggleSwitchChange() {
                 '                    </p>\n' +
                 '                </div>\n' +
                 '            </div>';
-        } else { //work
+        } else {
             container.innerHTML = '<div class="container container-left">\n' +
                 '                <div class="image" style="background-image:url(\'https://roztocze.net/news_foto/2021/0520/123244.jpg"></div>\n' +
                 '                <div class="content">\n' +
